5c48d41ecbb7d9f47e2cb6a8788d7c3892fde14f
kmutamar <khozin.mutamar@gmail.com>
2026-06-13 18:11:56 +0700

    Update 03_PROJECT_KULIAH__Data_Zakat.py

--- pages/03_PROJECT_KULIAH__Data_Zakat.py
+++ pages/03_PROJECT_KULIAH__Data_Zakat.py
@@ -35,6 +35,13 @@ tab1, tab2, tab3 = st.tabs(
 
 with tab1:
     st.title("Data dan Kurva")
+    st.write("""Sumber data:  Ria Indah Sari, 
+    Tomi Z., 
+    Era Napra Tilopa Sihombing, Mahiroh, Rado Yendra, Arisman Adnan, 
+    The Analysist Impacts of Covid-19 on Zakat, Revenue in Indonesia using an Exponential, Smoothing Model, 
+    International Journal of Mathematics Trends and Technology, 
+    Vol 68, Issue 5, 2022.'
+   """)
     st.header('Data')
     #st.table(df)
     st.dataframe(df)

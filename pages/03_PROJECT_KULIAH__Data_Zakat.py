cbe796ea7d42e55256e19123f65ea6cacc61397c
kmutamar <110453757+kmutamar@users.noreply.github.com>
2026-06-13 11:54:48 +0700

    Update 03_PROJECT_KULIAH__Data_Zakat.py

--- pages/03_PROJECT_KULIAH__Data_Zakat.py
+++ pages/03_PROJECT_KULIAH__Data_Zakat.py
@@ -1,15 +1,29 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import time
 from config.setup_page import logo
 logo()
+
+from metode.dichotomous_PON import dichotomous_pon
+from fungsi.fungsi_error_zakat import fungsi_error_zakat
 
 # --------------------
 # import metode dan fungsi
 # --------------------
 print("LOADING project kuliah...")
 
+data = np.loadtxt(
+    "data/dataZakat2.txt",
+    skiprows=1
+)
+data_t=data[:,0]
+data_f=data[:,1]
+df = pd.DataFrame(
+    data,
+    columns=["Waktu","Penerimaan"]
+)
 
 
 
@@ -23,13 +37,47 @@ with tab1:
     st.title("Disini halaman data dan kurva")
     #st.write(newton)
 
+    fig, ax = plt.subplots(figsize=(6,4))
+
+    ax.plot(data_t,data_f,'or',label='Data')
+    #ax.plot(data_t,fungsi_tebakan,'-b',label='Model')
+
+    ax.set_xlabel('Waktu')
+    ax.set_ylabel('Zakat')
+    ax.grid(True)
+    ax.legend()
+
+    st.pyplot(fig)
+    
+    #st.table(df)
+    st.dataframe(df)
+
 with tab2:
     st.title("Disini halaman pendekatan")
     #st.write(newton)
 
 with tab3:
-    st.title("Disini halaman simulasi pendekatan linier")
-    #st.write(newton)
+    hasil=dichotomous_pon(fungsi_error_zakat,[0,10],1e-6,data_t,data_f)
+    a=hasil[0]
+    st.write('a = ',a)
+    st.write('Hampiran = 68.39 exp(',a,'t)')
+    def f_tebakan(k,a,t):
+        return k*np.exp(a*t)
+    k=68.39
+    t=np.arange(0,20)
+    fungsi_tebakan=f_tebakan(k,a,t)
+    
+    fig, ax = plt.subplots(figsize=(6,4))
+
+    ax.plot(data_t,data_f,'or',label='Data')
+    ax.plot(data_t,fungsi_tebakan,'-b',label='Model')
+
+    ax.set_xlabel('Waktu')
+    ax.set_ylabel('Zakat')
+    ax.grid(True)
+    ax.legend()
+
+    st.pyplot(fig)    
         
                
 
